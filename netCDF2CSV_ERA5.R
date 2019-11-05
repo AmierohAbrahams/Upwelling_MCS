4d804b3af6fb1cf8f4be386f237292a145d11ffe
AmierohAbrahams <3559244@myuwc.ac.za>
2019-11-05 14:24:16 +0200

    update

--- netCDF2CSV_ERA5.R
+++ netCDF2CSV_ERA5.R
@@ -20,118 +20,73 @@ library(doMC); doMC::registerDoMC(cores = 7)
 # # 12345678901234567890123456789012345678901
 # # wu_daily08-19.nc
 
-ncFile <- '/home/amieroh/Documents/Data/Datasets/ERA5/u_v_seperate/wu_daily08-19.nc'  
-nc <- nc_open(ncFile)
-fNameStem <-
-  substr(basename(ncFile), 1, 13)
-u_10 <- ncvar_get(nc, varid = "u10") %>%
-  round(4)
-dimnames(u_10) <- list(lon = nc$dim$lon$vals,
-                         lat = nc$dim$lat$vals,
-                         time = nc$dim$time$vals)
-nc_close(nc)
-u_10 <- as_tibble(melt(u_10, value.name = "u_10"))
-u_10$time <- as.POSIXct(u_10$time, origin = "1970-01-01")
-
-
-
-###### v-wind 2008-2019
-# #          1         2         3         4
-# # 12345678901234567890123456789012345678901
-# # wu_daily08-19.nc
-
-ncFile <- '/home/amieroh/Documents/Data/Datasets/ERA5/u_v_seperate/wv_daily08-19.nc'
-nc <- nc_open(ncFile)
-fNameStem <-
-  substr(basename(ncFile), 1, 13)
-v_10 <- ncvar_get(nc, varid = "v10") %>%
-  round(4)
-dimnames(v_10) <- list(lon = nc$dim$lon$vals,
-                       lat = nc$dim$lat$vals,
-                       time = nc$dim$time$vals)
-nc_close(nc)
-v_10 <- as_tibble(melt(v_10, value.name = "v_10"))
-v_10$time <- as.POSIXct(v_10$time, origin = "1970-01-01")
-
-#####################################################################################
-### Extracting 1981 - 2007 u10
-# #          1         2         3         4
-# # 12345678901234567890123456789012345678901
-# # wind_daily81-99_165.nc
 
 ncFile <- '/home/amieroh/Documents/Data/Datasets/ERA5/wind_daily81-99_165.nc'
 nc <- nc_open(ncFile)
-fNameStem <-
-  substr(basename(ncFile), 1, 19)
+fNameStem <- substr(basename(ncFile), 1, 13)
 u_10 <- ncvar_get(nc, varid = "u10") %>%
   round(4)
 dimnames(u_10) <- list(lon = nc$dim$lon$vals,
                        lat = nc$dim$lat$vals,
                        time = nc$dim$time$vals)
 nc_close(nc)
-u_10 <- as_tibble(melt(u_10, value.name = "u_10"))
+u_10_df <- as_tibble(melt(u_10, value.name = "u_10"))
+u_10_df$time <- as.POSIXct(u_10_df$time * 3600, origin = "1900-01-01")
 
-ncFile <- '/home/amieroh/Documents/Data/Datasets/ERA5/wind_daily00-07_165.nc'
-nc <- nc_open(ncFile)
-fNameStem <-
-  substr(basename(ncFile), 1, 13)
-u_10 <- ncvar_get(nc, varid = "u10") %>%
-  round(4)
-dimnames(u_10) <- list(lon = nc$dim$lon$vals,
-                       lat = nc$dim$lat$vals,
-                       time = nc$dim$time$vals)
-nc_close(nc)
-u_10 <- as_tibble(melt(u_10, value.name = "u_10"))
-
-
-wind_u10_1999_2007 <- u10 %>% 
-  cbind(u10_2)
-
-#############################################################################3
-### Extracting 1981 - 2007 u10
-# #          1         2         3         4
-# # 12345678901234567890123456789012345678901
-# # wind_daily81-99_166.nc
 
 ncFile <- '/home/amieroh/Documents/Data/Datasets/ERA5/wind_daily81-99_166.nc'
 nc <- nc_open(ncFile)
-fNameStem <-
-  substr(basename(ncFile), 1, 19)
-v_10 <- ncvar_get(nc, varid = "v10") %>%
+fNameStem <- substr(basename(ncFile), 1, 19)
+v_10.2_df <- ncvar_get(nc, varid = "v10") %>%
   round(4)
-dimnames(v_10) <- list(lon = nc$dim$lon$vals,
-                       lat = nc$dim$lat$vals,
-                       time = nc$dim$time$vals)
+dimnames(v_10.2_df) <- list(lon = nc$dim$lon$vals,
+                            lat = nc$dim$lat$vals,
+                            time = nc$dim$time$vals)
 nc_close(nc)
-v_10 <- as_tibble(melt(v_10, value.name = "v_10"))
+v_10.2_df <- as_tibble(melt(v_10.2, value.name = "v_10"))
+v_10.2_df$time <- as.POSIXct(v_10.2_df$time * 3600, origin = "1900-01-01")
 
-ncFile <- '/home/amieroh/Documents/Data/Datasets/ERA5/wind_daily00-07_166.nc'
+
+ncFile <- '/home/amieroh/Documents/Data/Datasets/ERA5/wind_daily_165.nc'
 nc <- nc_open(ncFile)
-fNameStem <-
-  substr(basename(ncFile), 1, 13)
-v_10 <- ncvar_get(nc, varid = "v10") %>%
+fNameStem <- substr(basename(ncFile), 1, 14)
+u_10.2 <- ncvar_get(nc, varid = "u10") %>%
   round(4)
-dimnames(v_10) <- list(lon = nc$dim$lon$vals,
-                       lat = nc$dim$lat$vals,
-                       time = nc$dim$time$vals)
+dimnames(u_10.2) <- list(lon = nc$dim$lon$vals,
+                         lat = nc$dim$lat$vals,
+                         time = nc$dim$time$vals)
 nc_close(nc)
-v_10_2 <- as_tibble(melt(v_10, value.name = "v_10"))
+u_10.2_df <- as_tibble(melt(u_10.2, value.name = "u_10"))
+u_10.2_df$time <- as.POSIXct(u_10.2_df$time * 3600, origin = "1900-01-01")
+#######
+
+ncFile <- '/home/amieroh/Documents/Data/Datasets/ERA5/u_v_seperate/wu_daily08-19.nc'
+nc <- nc_open(ncFile)
+fNameStem <- substr(basename(ncFile), 1, 14)
+u_10.3_df <- ncvar_get(nc, varid = "u10") %>%
+  round(4)
+dimnames(u_10.3_df) <- list(lon = nc$dim$lon$vals,
+                            lat = nc$dim$lat$vals,
+                            time = nc$dim$time$vals)
+nc_close(nc)
+u_10.3_df <- as_tibble(melt(u_10.3_df, value.name = "u_10"))
+u_10.3_df$time <- as.POSIXct(u_10.3_df$time * 3600, origin = "1900-01-01")
+#####
+
+ncFile <- '/home/amieroh/Documents/Data/Datasets/ERA5/u_v_seperate/wv_daily08-19.nc'
+nc <- nc_open(ncFile)
+fNameStem <- substr(basename(ncFile), 1, 14)
+v_10.3_df <- ncvar_get(nc, varid = "v10") %>%
+  round(4)
+dimnames(v_10.3_df) <- list(lon = nc$dim$lon$vals,
+                            lat = nc$dim$lat$vals,
+                            time = nc$dim$time$vals)
+nc_close(nc)
+v_10.3_df <- as_tibble(melt(v_10.3_df, value.name = "u_10"))
+v_10.3_df$time <- as.POSIXct(v_10.3_df$time * 3600, origin = "1900-01-01")
 
 
-wind_v10_1999_2007 <- v10 %>% 
-  cbind(v10_2)
-
-
-
-
-
-
-
-
-
-
-
-
+ERA_5_wind <- rbind(u_10_df, u_10.2_df, u_10.3_df)
 
 
 
