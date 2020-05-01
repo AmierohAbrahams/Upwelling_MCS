9bf7aeb78a2abe7e16dc12841056dbc0f3365ba8
AmierohAbrahams <3559244@myuwc.ac.za>
2020-05-01 13:11:37 +0200

    update

--- SST_patterns.R
+++ SST_patterns.R
@@ -13,34 +13,23 @@ library(lubridate)
 library(ggpubr)
 library(zoo)
 library(FNN)
-library(scales)
-library(gridExtra)
 library(circular)
 library(fossil)
-library(mapproj)
 library(broom)
-# library(ncdf4) # This was used to process NetCDF files in an earlier version
-library(stringr)
 library(doMC); doMC::registerDoMC(cores = 4)
 library(fasttime)
-library(xtable)
-library(ncdf4) # library for processing netCDFs
 library(data.table)
-# library(plyr) # RWS: Never load the plyr package as it interferes with the tidyverse
 library(heatwaveR)
-library(grid)
-library(ggthemes)
-
 # 2: Loading data ----------------------------------------------------
 
-load("Data/site_list_sub.Rdata")
+# load("Data/site_list_sub.Rdata")
 load("Data/SACTN_US.RData")
 #load("Data/site_pixels.RData") # 5 decimal places
 load("Data/OISST.RData") # 2 decimal places
 OISST <- BC_avhrr_only_v2_Document_Document 
 rm(BC_avhrr_only_v2_Document_Document ); gc()
 load("Data/CMC.RData") # 1decimal places
-load("Data/MUR.RData")
+load("Data/_coast_angleMUR.RData")
 load("Data_coast_angle/site_pixels.RData")
 
 # ### The temperature products
@@ -56,8 +45,19 @@ CMC_prod <- CMC %>%
   unique() %>%
   mutate(product = "CMC")
 
-sat_data <- rbind(CMC_prod, OISST_prod) %>% 
+MUR_prod <- MUR_new %>%
+  dplyr::select(lon, lat) %>%
+  unique() %>%
+  mutate(product = "MUR")
+
+G1SST_prod <- G1SST %>%
+  dplyr::select(lon, lat) %>%
+  unique() %>%
+  mutate(product = "G1SST")
+
+sat_data <- rbind(MUR_prod, OISST_prod) %>% 
   dplyr::select(product, lon, lat)
+
 sat_pixels <- sat_data %>%
   dplyr::select(product, lon, lat) %>%
   unique()
@@ -68,10 +68,10 @@ match_func <- function(df){
   OISST_index <- OISST_prod[as.vector(knnx.index(as.matrix(OISST_prod[,c("lon", "lat")]),
                                                  as.matrix(df[,c("lon_site", "lat_site")]), k = 1)),] %>%
     cbind(., df)
-  CMC_index <- CMC_prod[as.vector(knnx.index(as.matrix(CMC_prod[,c("lon", "lat")]),
+  MUR_index <- MUR_prod[as.vector(knnx.index(as.matrix(MUR_prod[,c("lon", "lat")]),
                                              as.matrix(df[,c("lon_site", "lat_site")]), k = 1)),] %>%
     cbind(., df)
-  res <- rbind(OISST_index, CMC_index)
+  res <- rbind(OISST_index, MUR_index)
   return(res)
 }
 
@@ -79,48 +79,41 @@ pixel_match <- site_pixels %>%
   group_by(site) %>%
   group_modify(~match_func(.x))
 
+
 OISST_fill <- right_join(OISST, filter(pixel_match, product == "OISST"), by = c("lon", "lat"))
-CMC_fill <- right_join(CMC, filter(pixel_match, product == "CMC"), by = c("lon", "lat"))
-rm(OISST, CMC); gc()
+MUR_fill <- right_join(MUR_new, filter(pixel_match, product == "MUR"), by = c("lon", "lat"))
+rm(OISST, MUR); gc()
 
 selected_sites <- c("Port Nolloth", "Lamberts Bay", "Sea Point", "Saldanha Bay")
 
 OISST_fill <- OISST_fill %>%
   filter(site %in% selected_sites)
 
-CMC_fill <- CMC_fill %>%
+MUR_fill <- MUR_fill %>%
   filter(site %in% selected_sites)
 
 # save(OISST_fill, file = "Data_coast_angle/OISST_fill.RData")
 # save(CMC_fill, file = "Data_coast_angle/CMC_fill.RData")
+# save(MUR_fill, file = "Data_coast_angle/MUR_fill.RData")
+# save(G1SST_fill, file = "Data_coast_angle/G1SST_fill.RData")
 
 # load("Data/UI_angle.RData")
-load("Data_coast_angle/UI_angle.RData")
+load("Data_coast_angle/UI_angle.RData") # Created in script 'upwell_IDX.Rmd'
 
-upwelling <- UI_angle %>%
-  mutate(date = as.Date(date)) %>% 
-  dplyr::rename(temp = ui.saws) %>%
-  group_by(site) %>%
-  nest() %>% # apply the following functions to all of the variables in te dataset
-  mutate(clim = purrr::map(data, ts2clm, climatologyPeriod = c("1992-07-09", "2017-12-21")), # creating a column of climatologies. Column will be named clim
-                 exceed = purrr::map(clim, exceedance, minDuration = 1, threshold = 1)) %>%  #Upwelling cannot be descrbed as an event. Upwelling can last for a few hours. Given that we have daily data, upwelling events minimum duration here will be 1day
-  select(-data, -clim) %>%
-  unnest() %>%
-  filter(row_number() %% 2 == 1) %>%
-  unnest() %>% # creates a column for each variables
-  dplyr::rename(ui.saws = temp) %>% # rename upwelling index vale to temp so that it could work with the function
-  select(site, t, ui.saws, exceedance)
+upwelling <- UI_angle %>% 
+  mutate(exceedance = ifelse(ui.saws >= 1, TRUE, FALSE),
+         t = as.Date(t))
 
 detect_event_custom <- function(df){
   res <- detect_event(df, threshClim2 = df$exceedance, minDuration = 1, coldSpells = T)$event # 1 or 3?
   return(res)
 }
-# 
+
 ts2clm_custom <- function(df){
-  res <- ts2clm(df, pctile = 25, climatologyPeriod = c("2010-06-23", "2014-06-30"))
+  res <- ts2clm(df, pctile = 25, climatologyPeriod = c("2003-01-01", "2015-12-30"))
   return(res)
 }
-# 
+
 # Calculate the upwelling event metrics
 upwelling_detect_event <- function(df){
   upwell_base <- df %>%
@@ -132,18 +125,17 @@ upwelling_detect_event <- function(df){
     group_by(site, product, heading, distance, lon, lat) %>%
     group_modify(~detect_event_custom(.x))
 }
-# 
+
 OISST_upwell_base <- upwelling_detect_event(df = OISST_fill)
 #save(OISST_upwell_base, file = "Data/OISST_upwell_base.RData")
 save(OISST_upwell_base, file = "Data_coast_angle/OISST_upwell_base.RData")
 CMC_upwell_base <- upwelling_detect_event(df = CMC_fill)
-save(CMC_upwell_base, file = "Data/CMC_upwell_base.RData")
 save(CMC_upwell_base, file = "Data_coast_angle/CMC_upwell_base.RData")
-MUR_upwell_base <- upwelling_detect_event(df = Final)
+MUR_upwell_base <- upwelling_detect_event(df = MUR_fill)
 save(MUR_upwell_base, file = "Data_coast_angle/MUR_upwell_base.RData")
 
 # Here we remove the site Hout Bay so that we have a long time series. The length of Hout Bay time series ends in 200. Many sites change from here
-SACTN_US <- SACTN_US %>%
+SACTN_US <- SACTN_US %>% 
   filter(site %in% selected_sites)
 
 load("Data/site_list_v4.2.RData")
@@ -184,21 +176,5 @@ CMC_final <- removing_distance_func(df = CMC_upwell_base)
 # save(G1SST_final, file = "Data/G1SST_final.RData")
 # save(MUR_final, file = "Data/MUR_final.RData")
 # save(CMC_final, file = "Data/CMC_final.RData")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
